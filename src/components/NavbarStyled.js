298d1c2e0d6ac31d2c0978f53fb9d7064641651c
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-09-09 23:19:32 -0300

    Added position fixed to navbar

--- src/components/NavbarStyled.js
+++ src/components/NavbarStyled.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 const Navbar = styled.div`
     height: 70px;
+    width: 100%;
+    box-sizing: border-box;
     padding-left: 18px;
     padding-right: 10px;
     background-color: #126BA5;
@@ -9,6 +11,10 @@ const Navbar = styled.div`
     align-items: center;
     justify-content: space-between;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
 
     & > .profile-picture {
         width: 51px;
